6d2d91478d35733fce280f7fab52dc177947821a
jpdotfranca <jpdotfranca@gmail.com>
2020-10-18 14:23:59 -0300

    up

---
+++ Teste/Runner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Teste
+{
+    public class Runner
+    {
+        private static float _n = 3.14159f;
+
+        public static float AreaDoCirculo(float R)
+        {
+            float area = (float)(_n * Math.Pow(R,2));
+            return area;
+        }
+    }
+}
--- Teste/UnitTest1.cs
+++ Teste/UnitTest1.cs
@@ -14,5 +14,33 @@ namespace Teste
         {
             Assert.Pass();
         }
+
+        [Test]
+        public void Teste1()
+        {
+            float R = 2;
+            float saidaEsperada = 12.5664f;
+            float resultado = Runner.AreaDoCirculo(R);
+            Assert.AreEqual(resultado, saidaEsperada);
+        }
+
+        [Test]
+        public void Teste2()
+        {
+            float R = 100.64f;
+            float saidaEsperada = 31819.3103f;
+            float resultado = Runner.AreaDoCirculo(R);
+            Assert.AreEqual(resultado, saidaEsperada);
+        }
+
+              [Test]
+        public void Teste3()
+        {
+            float R = 150.00f;
+            float saidaEsperada = 70685.7750f;
+            float resultado = Runner.AreaDoCirculo(R);
+            Assert.AreEqual(resultado, saidaEsperada);
+        }
+
     }
 }
